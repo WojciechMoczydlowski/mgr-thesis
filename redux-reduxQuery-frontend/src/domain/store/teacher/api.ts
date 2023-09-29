0c491fe0e27e6944e98581d42f5b6d17a245f1c8
Wojciech Moczydlowski <wojciech.moczydlowski98@gmail.com>
2023-09-29 16:34:58 +0200

    Add exam CRUD

--- redux-reduxQuery-frontend/src/domain/store/teacher/api.ts
+++ redux-reduxQuery-frontend/src/domain/store/teacher/api.ts
@@ -1,11 +1,13 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { Course } from "./courses/model";
 import Cookies from "js-cookie";
+import { Exam } from "@/domain/teacher/exams/model/exam";
+import { PostExamPayload, PutExamPayload } from "./model";
 
 // Define a service using a base URL and expected endpoints
 export const teacherApi = createApi({
   reducerPath: "teacherApi",
+  tagTypes: ["Exams"],
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:8080/api/v1/teacher/",
     prepareHeaders(headers) {
@@ -19,12 +21,68 @@ export const teacherApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTeacherCourses: builder.query<Course[], void>({
+    getTeacherCourses: builder.query<Exam[], void>({
       query: () => `courses`,
+    }),
+    // Exams
+    getTeacherExams: builder.query<Exam[], { courseId: string }>({
+      query: ({ courseId }) => `courses/${courseId}`,
+      providesTags: (result) => [
+        { type: "Exams", id: "LIST" },
+        ...(result
+          ? result.map(({ id }) => ({ type: "Exams", id } as const))
+          : []),
+      ],
+    }),
+    postTeacherExam: builder.mutation<
+      void,
+      { courseId: string; payload: PostExamPayload }
+    >({
+      query({ courseId, payload }) {
+        return {
+          url: `courses/${courseId}`,
+          method: "POST",
+          body: payload,
+        };
+      },
+      invalidatesTags: [{ type: "Exams", id: "LIST" }],
+    }),
+    putTeacherExam: builder.mutation<
+      void,
+      { examId: string; courseId: string; payload: PutExamPayload }
+    >({
+      query({ examId, courseId, payload }) {
+        return {
+          url: `courses/${courseId}/${examId}`,
+          method: `PUT`,
+          body: payload,
+        };
+      },
+      invalidatesTags: (_, __, arg) => [{ type: "Exams", id: arg.examId }],
+    }),
+    deleteTeacherExam: builder.mutation<
+      void,
+      { examId: string; courseId: string }
+    >({
+      query({ examId, courseId }) {
+        return {
+          url: `courses/${courseId}/${examId}`,
+          method: `DELETE`,
+        };
+      },
+      invalidatesTags: (_, __, arg) => [{ type: "Exams", id: arg.examId }],
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetTeacherCoursesQuery } = teacherApi;
+export const {
+  // courses
+  useGetTeacherCoursesQuery,
+  // exams
+  useGetTeacherExamsQuery,
+  usePostTeacherExamMutation,
+  usePutTeacherExamMutation,
+  useDeleteTeacherExamMutation,
+} = teacherApi;
---
+++ redux-reduxQuery-frontend/src/domain/store/teacher/model.ts
@@ -0,0 +1,15 @@
+export type PostExamPayload = {
+  title: string;
+  description: string;
+  dateTimeStart: string;
+  dateTimeEnd: string;
+  duration: number;
+};
+
+export type PutExamPayload = {
+  title: string;
+  description: string;
+  dateTimeStart: string;
+  dateTimeEnd: string;
+  duration: number;
+};
